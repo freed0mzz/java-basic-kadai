b2c7995a4e4d194670cbdb89fd08d089d0e920f9
freed0mzz.gif <chuleekorn.gif@gmail.com>
2026-03-24 16:48:18 +0900

    Add files via upload

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,16 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+    public static void main(String[] args) {
+
+        // インスタンス生成
+        Car_Chapter15 car = new Car_Chapter15();
+
+        // ギアを3に変更
+        car.changeGear(3);
+
+        // 走行
+        car.run();
+    }
+}
